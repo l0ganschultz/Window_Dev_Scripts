96b8aa991586a69e499ca74499dd6f71680d4a8a
Logan Schultz <logan.schultzemail@gmail.com>
2025-12-17 16:51:16 -0600

    Golden Password Reset | V1.0

---
+++ Golden Ticket/Goldenpass1.0.ps1
@@ -0,0 +1,11 @@
+﻿#--------------------------------------------------------------
+# Golden Ticket Password Reset | AD Server
+# Made by Logan Schultz
+#Version | 1.0
+#--------------------------------------------------------------
+
+#Last Reset Time
+#--------------------------------------------------------------
+Get-ADUser "Krbtgt" -Property PasswordLastSet
+$AccountPassword = Read-Host "Enter new password for Krbtgt" -AsSecureString
+Set-ADAccountPassword -Identity "Krbtgt" -Reset -NewPassword $AccountPassword
