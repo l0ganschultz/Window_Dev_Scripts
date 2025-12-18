687b273a0565ae0f2a24aa3fcf1e45ff57888b6e
Logan Schultz <logan.schultzemail@gmail.com>
2025-12-17 18:10:39 -0600

    Backup Script | V1.0
    Backups scripts for DNS, Firewalls, Audit Policy, Registry, and web Server Backups

---
+++ Backups/backup1.0.ps1
@@ -0,0 +1,53 @@
+﻿#--------------------------------------------------------------
+# Back UP Script
+# Made by Logan Schultz
+# Version | 1.0
+#--------------------------------------------------------------
+
+#--------------------------------------------------------------
+#Back ups
+#--------------------------------------------------------------
+
+#DNS
+#--------------------------------------------------------------
+New-Item -ItemType Directory -Path "C:\Backups\DNS" -Force
+$backupPath = "C:\Backups\DNS\DNS_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss')"
+New-Item -ItemType Directory -Path $backupPath -Force
+Copy-Item "C:\Windows\System32\dns\*" $backupPath -Recurse -Force
+
+#Security Policys
+New-Item -ItemType Directory -Path "C:\Backups\LocalSecurity" -Force
+$path = "C:\Backups\LocalSecurity\LocalSecurityPolicy_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').inf"
+secedit /export /cfg $path
+
+
+#Firewall
+#--------------------------------------------------------------
+New-Item -ItemType Directory -Path "C:\Backups\Firewall" -Force
+$path = "C:\Backups\Firewall\Firewall_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss').wfw"
+netsh advfirewall export $path
+
+#Audit Policy
+#--------------------------------------------------------------
+New-Item -ItemType Directory -Path "C:\Backups\Audit" -Force
+#Backup-AuditPolicy -Path C:\Backups\Audit
+
+#Registry
+#--------------------------------------------------------------
+$ts = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
+New-Item -ItemType Directory -Path "C:\Backups\Registry" -Force
+New-Item -ItemType Directory -Path "C:\Backups\Registry\Registry_$ts" -Force
+
+reg export HKLM "C:\Backups\Registry\Registry_$ts\HKLM.reg" /y
+reg export HKCU "C:\Backups\Registry\Registry_$ts\HKCU.reg" /y
+reg export HKCR "C:\Backups\Registry\Registry_$ts\HKCR.reg" /y
+reg export HKU  "C:\Backups\Registry\Registry_$ts\HKU.reg"  /y
+reg export HKCC "C:\Backups\Registry\Registry_$ts\HKCC.reg" /y
+
+#Web Server Backups
+#--------------------------------------------------------------
+New-Item -ItemType Directory -Path "C:\Backups\Web" -Force
+$backupPathWEB = "C:\Backups\Web\Web_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss')"
+New-Item -ItemType Directory -Path $backupPathWEB -Force
+Copy-Item "C:\inetpub\*" $backupPathWEB -Recurse -Force
+
